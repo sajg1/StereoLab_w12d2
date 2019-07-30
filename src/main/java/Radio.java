fd50aa91ad7e6294bebb7e78f4ece559e69380c5
Steve Gossip <stephengossip@gmail.com>
2019-07-30 12:51:12 +0100

    passed test for radio make

---
+++ src/main/java/Radio.java
@@ -0,0 +1,18 @@
+public class Radio {
+
+    private String make;
+    private String model;
+
+    Radio(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RadioTest {
+
+    Radio radio;
+
+    @Before
+    public void before() {
+        radio = new Radio("Sony", "A6000");
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Sony", radio.getMake());
+    }
+}
